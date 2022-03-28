e515a6bd8952ca918269582b190f3d76fc6f8c5d
amanrajput2001 <90300203+amanrajput2001@users.noreply.github.com>
2022-03-28 16:20:41 +0530

    Update thumbnails.py

--- Tamilini/Utilities/thumbnails.py
+++ Tamilini/Utilities/thumbnails.py
@@ -24,13 +24,13 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
                 await f.write(await resp.read())
                 await f.close()
     image1 = Image.open(f"cache/thumb{userid}.jpg")
-    image2 = Image.open(f"Utils/{theme}.png")
+    image2 = Image.open(f"Utils/{theme}.PNG")
     image3 = changeImageSize(1280, 720, image1)
     image4 = changeImageSize(1280, 720, image2)
     image5 = image3.convert("RGBA")
     image6 = image4.convert("RGBA")
     Image.alpha_composite(image5, image6).save(f"cache/temp{userid}.png")
-    img = Image.open(f"Utils/red.png")
+    img = Image.open(f"cache/temp{userid}.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype("Utils/finalfont.ttf", 85)
     font2 = ImageFont.truetype("Utils/finalfont.ttf", 60)
@@ -53,5 +53,5 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
     img.save(f"cache/final{userid}.png")
     os.remove(f"cache/temp{userid}.png")
     os.remove(f"cache/thumb{userid}.jpg")
-    final = f"Utils/red.png"
+    final = f"cache/final{userid}.png"
     return final
